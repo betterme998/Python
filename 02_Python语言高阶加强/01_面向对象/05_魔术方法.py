0d51e06e7a800e8b61007e1dfc4d0366d721fbd3
betterme <betterme998@163.com>
2026-04-12 20:57:37 +0800

    魔术方法和封装

--- 02_Python语言高阶加强/01_面向对象/05_魔术方法.py
+++ 02_Python语言高阶加强/01_面向对象/05_魔术方法.py
@@ -12,8 +12,18 @@ class Student:
     return f"Srudent类对象, name:{self.name}, age:{self.age}"
   
   # __lt__  魔术方法 小于、大于符号比较 (两个对象比较)
-  def __lt__(self, other):
+  def __lt__(self, other): # other 另一个类对象
     return self.age < other.age
+  
+  # __le__ 魔术方法 <=, >=两种比较运算符上
+  def __le__(self, other): #必传self，other另一个类对象
+    return self.age <= other.age
+  
+  # __eq__  魔术方法 等于符号比较
+  # 不实现__eq__方法，比较内存地址，即比较是否同一个对象  
+  # 实现__eq__方法，就可以按照自己的想法决定2个对象是否相同
+  def __eq__(self, other):
+    return self.age == other.age
 
 
 
@@ -33,11 +43,20 @@ print(str(stu)) # Srudent类对象, name:周杰伦, age:18
 返回值：True / False
 内容：自行定义
 """
-stu2 = Student("林俊杰", 19)
+stu2 = Student("林俊杰", 18)
 print(stu < stu2) # True
 print(stu > stu2) # False
 
 
 
 # __le__  魔术方法 小于等于、大于等于符号比较
+#传入参数：other，另一个类对象
+#返回值：True / False
+print(stu <= stu2)# True
+print(stu >= stu2)# False
+
+
+
 # __eq__  魔术方法 等于符号比较
+print(stu == stu2) # true
+
